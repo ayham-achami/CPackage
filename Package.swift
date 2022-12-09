58403c13eec3587572a1076754c35c271104e086
Eugeniy Zadumin <zadumin@dev.vtb.ru>
2022-12-09 18:40:41 +0400

    add CoreUI module

--- Package.swift
+++ Package.swift
@@ -25,6 +25,10 @@ let package = Package(
         .binaryTarget(
             name: "Integration",
             url: "https://nexus.inno.tech/repository/vkc-hosted-raw-ios/com/innotech/Integration/496814869/Integration-27843082.496814869.xcframework.zip",
-            checksum: "48720a196ccf5db819f4aa0f61acae7a798573b883b539fe8e5e81a60cfc29b4")
+            checksum: "48720a196ccf5db819f4aa0f61acae7a798573b883b539fe8e5e81a60cfc29b4"),
+        .binaryTarget(
+            name: "CoreUI",
+            url: "https://nexus.inno.tech/repository/vkc-hosted-raw-ios/com/innotech/CoreUI/435867645/CoreUI-27843187.435867645.xcframework.zip",
+            checksum: "7411f5f5e01b467cb7c4f3186dbcb074dd381443f24aa1957557260150ee37f6")
     ]
 )
