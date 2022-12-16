a68374f25db3a4ea3b56ff9ac7ce5e5069c259ed
Kirill Selivanov <sks97@yandex.ru>
2022-12-16 10:59:12 +0300

    Added Core-Network, Integration, CoreUI targets

--- Package.swift
+++ Package.swift
@@ -7,19 +7,66 @@ let package = Package(
     name: "CPackage",
     platforms: [.iOS(.v13), .macCatalyst(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CPackage",
-            targets: [
-                "CoreNetwork"
-            ]
-        )
+        .library(name: "CoreUI", targets: ["CoreUIWrapper"]),
+        .library(name: "CoreNetwork", targets: ["CoreNetworkWrapper"]),
+        .library(name: "Integration", targets: ["IntegrationWrapper"]),
+        .library(name: "RealmFramework", targets: ["RealmFrameworkWrapper"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ayham-achami/CRest", branch: "mainline"),
+        .package(url: "https://github.com/ayham-achami/CArch", branch: "mainline"),
+        .package(url: "https://github.com/ayham-achami/CUIKit.git", branch: "mainline"),
+        .package(url: "https://github.com/ayham-achami/CRepository", branch: "mainline"),
+        .package(url: "https://github.com/ayham-achami/CFoundation.git", branch: "mainline"),
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.2.0"))
+    ],
     targets: [
+        // RealmFramework
+        .target(name: "RealmFrameworkWrapper",
+                dependencies: [.target(name: "RealmFramework")],
+                path: "RealmFrameworkWrapper"),
+        .binaryTarget(
+            name: "RealmFramework",
+            url: "https://nexus.inno.tech/repository/vkc-hosted-raw-ios/com/innotech/RealmFramework/RealmFramework.xcframework.zip",
+            checksum: "7ec4e064b5575176164a281b8b1f87dad465f05b2e95257f59e5586363a5b11f"),
+        // CoreNetwork
+        .target(name: "CoreNetworkWrapper",
+                dependencies: [
+                    .target(name: "CoreNetwork"),
+                    .product(name: "CArch", package: "CArch"),
+                    .product(name: "CRest", package: "CRest"),
+                    .product(name: "CFoundation", package: "CFoundation"),
+                    .product(name: "AlamofireImage", package: "AlamofireImage")
+                ],
+                path: "CoreNetworkWrapper"),
         .binaryTarget(
             name: "CoreNetwork",
             url: "https://nexus.inno.tech/repository/vkc-hosted-raw-ios/com/innotech/CoreNetwork/526911581/CoreNetwork-27837641.526911581.xcframework.zip",
-            checksum: "ba6f15c9b98487952b17dfa4a5234342a5a7d1bc7b25fa89803cdde7d619a943")
+            checksum: "ba6f15c9b98487952b17dfa4a5234342a5a7d1bc7b25fa89803cdde7d619a943"),
+        // Integration
+        .target(name: "IntegrationWrapper",
+                dependencies: [
+                    .target(name: "Integration"),
+                    .target(name: "RealmFrameworkWrapper"),
+                    .target(name: "CoreNetworkWrapper"),
+                    .product(name: "CRepository", package: "CRepository")
+                ],
+                path: "IntegrationWrapper"),
+        .binaryTarget(
+            name: "Integration",
+            url: "https://nexus.inno.tech/repository/vkc-hosted-raw-ios/com/innotech/Integration/496814869/Integration-27843082.496814869.xcframework.zip",
+            checksum: "48720a196ccf5db819f4aa0f61acae7a798573b883b539fe8e5e81a60cfc29b4"),
+        // CoreUI
+        .target(name: "CoreUIWrapper",
+                dependencies: [
+                    .target(name: "CoreUI"),
+                    .target(name: "CoreNetworkWrapper"),
+                    .product(name: "CUIKit", package: "CUIKit")
+                ],
+                path: "CoreUIWrapper"),
+        .binaryTarget(
+            name: "CoreUI",
+            url: "https://nexus.inno.tech/repository/vkc-hosted-raw-ios/com/innotech/CoreUI/419514678/CoreUI-27857351.419514678.xcframework.zip",
+            checksum: "85213193d3bd3bab673a461be156f386fa1571cf2ab30b4e667eb44ff89a64bb"),
     ]
 )
